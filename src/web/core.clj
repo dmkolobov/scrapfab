2cbfc3cc1a8e1403473379f69ad122c935bfef08
Dmitry Kolobov <dmkolobov@gmail.com>
2016-12-30 01:58:41 -0700

    Create a static data store with namespaced keys.

--- src/web/core.clj
+++ src/web/core.clj
@@ -1,24 +1,62 @@
 (ns web.core
   (:require [stasis.core :as stasis]
-            [clojure.set :refer [rename-keys]]))
+            [clojure.set :refer [rename-keys]]
+            [clojure.pprint :refer [pprint]]
+            [clojure.java.io :as io]
+            [clojure.string]))
 
 (defn slurp-reagent!
+  "Slurp the compiled reagent app and return a map of pages for the javascript
+  assets."
   []
   (let [js (stasis/slurp-directory "resources/public/js/compiled/" #"\.js$")]
     (rename-keys js
                  (zipmap (keys js)
                          (map #(str "/js/compiled" %) (keys js))))))
 
+(defn basename
+  [f ext]
+  (clojure.string/replace (.getName f)
+                          ext
+                          ""))
+
+(defn filename->id
+  [coll-ns path]
+  (keyword (name coll-ns)
+           (basename (io/file path) ".edn")))
+
+(defn slurp-collection!
+  [coll-ns path]
+  (let [sources (stasis/slurp-directory path #"\.edn$")
+        names   (map #(filename->id coll-ns %) (keys sources))
+        items   (map read-string (vals sources))]
+    (zipmap names items)))
+
+(defn slurp-data!
+  []
+  (map (fn [coll-dir]
+         (let [path (.getPath coll-dir)
+               name (basename coll-dir "")]
+           (slurp-collection! name path)))
+       (let [fs (.listFiles (io/file "resources/public/data"))]
+         (println fs)
+         fs)))
+
 (def pages
   (merge {"/"
-            "<html>
+          (str "<html>
               <head>
                 <script type=\"text/javascript\" src=\"/js/compiled/web.js\"></script>
               </head>
               <body>
-                Hello, world!
+
+              <pre>"
+               (with-out-str
+                 (pprint
+                   (slurp-data!)))
+               "</pre>
               </body>
-            </html>"}
+            </html>")}
 
          (slurp-reagent!)))
 
--- src/web/photos.clj
+++
@@ -1,6 +0,0 @@
-(ns web.photos)
-
-(defrecord Photo
-  [src
-   photog-name
-   photog-website])
