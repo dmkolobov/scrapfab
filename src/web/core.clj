531e0e007fe8cbfd08d942d70495e3d5787f6fe0
Dmitry Kolobov <dmkolobov@gmail.com>
2017-01-06 22:47:55 -0700

    Implement scrapfab layout.

--- src/web/core.clj
+++ src/web/core.clj
@@ -40,43 +40,60 @@
         items   (map read-string (vals sources))]
     (zipmap names items)))
 
-(defn slurp-data!
-  []
-  (map (fn [coll-dir]
-         (let [path (.getPath coll-dir)
-               name (basename coll-dir "")]
-           (slurp-collection! name path)))
-       (.listFiles (io/file "resources/public/data"))))
+(defn scrapfab-menu
+  [current-url]
+  [:div.pure-menu.pure-menu-horizontal
+   [:ul.pure-menu-list
+    [:li.pure-menu-item [:a.pure-menu-link {:href "/"}      "Home"]]
+    [:li.pure-menu-item [:a.pure-menu-link {:href "/about"} "About"]]
+    [:li.pure-menu-item.pure-menu-has-children
+     [:a.pure-menu-link {:href "/fab"} "Fabrication"]
+     [:ul.pure-menu-children.pure-menu-allow-hover
+      [:li.pure-menu-item [:a.pure-menu-link {:href "/fab/art"}         "Fine Art"]]
+      [:li.pure-menu-item [:a.pure-menu-link {:href "/fab/residential"} "Residential"]]
+      [:li.pure-menu-item [:a.pure-menu-link {:href "/fab/commercial"}  "Commercial"]]]]]])
 
-(register-layout   :base
-                   (fn base-template
-                     [& {:keys [scripts stylesheets body message]}]
-                     [:html
-                      [:head
-                       (for [js scripts]      [:script {:type "text/javascript" :src js}])
-                       (for [css stylesheets] [:link {:rel "stylesheet" :href css}])]
-                      [:body
-                       body
-                       [:pre (str message)]]]))
+(register-layout :scrapfab-base
+                 (fn scrapfab-base
+                   [& {:keys [title body current-url js css]}]
+                   [:html
+                    [:head
+                     [:title title]]
+                    [:body
+                     [:div.pure-g
+                      [:div.pure-u-1-3 [:div.scrap-logo "scrap"]]
+                      [:div.pure-u-2-3 [:div.fab-logo   "fab"]]
+                      [:div.pure-u-1-3]
+                      [:div.pure-u-2-3 (scrapfab-menu current-url)]]
+
+                      body
+
+                     (for [js-url js]
+                       [:script {:type "text/javascript" :src  js-url}])
+                     (for [css-url css]
+                       [:link {:rel "stylesheet" :href css-url}])]]))
 
 (register-template :main
                    (fn main-view
                      [& _]
-                     [:h1 "HELLO, WORLD!"]))
+                     [:h1 "HELLO, WORLD!!"]))
 
 (def resources
-  {:scripts     ["/js/compiled/web.js"]
+  {:js     ["/js/compiled/web.js"]
 
-   :stylesheets ["https://unpkg.com/purecss@0.6.1/build/pure-min.css"
+   :css ["https://unpkg.com/purecss@0.6.1/build/pure-min.css"
+                 "https://unpkg.com/purecss@0.6.1/build/grids-min.css"
+                 "https://unpkg.com/purecss@0.6.1/build/grids-responsive-min.css"
                  "css/main.css"
                  "css/fonts.css"]})
 
 (def pages
-  (merge {"/" (hiccup/html
-                (render :layout   :base
-                        :template :main
-                        :data     (merge resources
-                                         {:message "foobar"})))}
+  (merge {"/" (str "<!DOCTYPE html>"
+                   (hiccup/html
+                    (render :layout   :scrapfab-base
+                            :template :main
+                            :data     (merge resources
+                                             {:message "foobar"}))))}
 
          (slurp-css!)
          (slurp-js!)))
