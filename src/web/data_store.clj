5781df7b18121a64f86381e07b6664d6aeb38263
Dmitry Kolobov <dmkolobov@gmail.com>
2017-01-25 12:11:54 -0700

    Create replacement map for query walking.

--- src/web/data_store.clj
+++ src/web/data_store.clj
@@ -1,15 +1,19 @@
 (ns web.data-store
   (:require [clojure.string :as string]
+            [clojure.walk :as walk]
             [ubergraph.core :as uber]
             [ubergraph.alg :as alg]
             [stasis.core :refer [slurp-directory]]
             [clojure.tools.reader.edn :as edn]))
 
 (defn drop-ext
+  "Given a path, retur"
   [path]
   (first (string/split path #"\.")))
 
 (defn path->ks
+  "Converts a file path to a keyword sequence as in the second argument
+   to 'assoc-in'."
   [path]
   (map keyword (rest (string/split (drop-ext path) #"/"))))
 
@@ -22,11 +26,11 @@
   (eduction (map (juxt first (comp render second)))
             (slurp-directory path (re-pattern (str "\\." ext)))))
 
-(defn read-roots
+(defn read-forms
   [configs]
   (transduce (map form-seq) (completing into) [] configs))
 
-;; ---------- ANALSIS ------------
+;; ---------- ANALISYS ------------
 
 (defn branch?
   [pred x]
@@ -38,19 +42,26 @@
           (tree-seq #(branch? pred %) seq form)))
 
 (defn analyze
-  [pred ks form]
-  (cond (pred form)
-        [[ks form]]
+  "Returns a sequence of [ks sub-form] tuples, where 'pred' returns true
+  for 'sub-form', and 'ks' is the path to 'sub-form' in the original
+  'form', as defined in 'get-in'. If the argument 'ks' is given,
+  it is used as a prefix to each path returned."
+  ([pred form]
+   (analyze pred [] form))
 
-        (map? form)
-        (transduce (map (fn [[k v]] (analyze pred (conj (vec ks) k) v)))
-                   (completing into)
-                   []
-                   form)
+  ([pred ks form]
+   (cond (pred form)
+         [[ks form]]
 
-        :default
-        (eduction (map #(vector ks %))
-                  (collect-forms pred form))))
+         (map? form)
+         (transduce (map (fn [[k v]] (analyze pred (conj (vec ks) k) v)))
+                    (completing into)
+                    []
+                    form)
+
+         :default
+         (eduction (map #(vector ks %))
+                   (collect-forms pred form)))))
 
 (defrecord PullForm [form file ks])
 
@@ -92,13 +103,25 @@
 
 ;; ---------- PUBLIC API -------------
 
+(defn add-context
+  [context [path form]]
+  (assoc-in context (path->ks path) form))
+
 (defn file-db
   [& configs]
-  (let [form-seq (read-roots configs)]
-    {:forms form-seq
-     :graph (->> form-seq
-                 (analyze-forms)
-                 (compile-graph))}))
+  (let [forms (read-forms configs)
+        graph (->> forms (analyze-forms) (compile-graph))]
+    {:forms   forms
+     :graph   graph
+     :context (reduce add-context {} forms)}))
+
+(defn resolve-db
+  [{:keys [graph context] :as db}]
+  (reduce (fn [smap {:keys [form]}]
+            (assoc smap
+              form (walk/postwalk-replace smap (get-in context (rest form)))))
+          {}
+          (reverse (alg/topsort graph))))
 
 (defn fx
   []
